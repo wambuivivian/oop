50eff4c3a9e6a6079975744ca63d5cce32871cfd
wambuivivian <miriivivianaa@gmail.com>
2026-07-21 12:02:55 +0300

    adding words

--- vehicle.py
+++ vehicle.py
@@ -5,4 +5,4 @@ class Vehicle:
         self.year = year
 
     def start_engine(self):
-        return f"the engine of the {self.year} {self.make} {self. model}is starting"    
+        return f"the engine of the {self.year} {self.make} {self. model}is stoping"
