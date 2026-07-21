c98e7fe275b49d70a2886a0c0a8d5674ad24a149
wambuivivian <miriivivianaa@gmail.com>
2026-07-21 12:18:47 +0300

    added new fanction dunder method

--- vehicle.py
+++ vehicle.py
@@ -6,3 +6,6 @@ class Vehicle:
 
     def start_engine(self):
         return f"the engine of the {self.year} {self.make} {self. model}is stoping"
+    
+    def __str__(self):
+        return f"{self.year} {self.make} {self.model}"
